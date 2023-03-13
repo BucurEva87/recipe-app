44f0dbc39d5b50c1571978e96b22b52757896560
Krishna <libraryismyparadise@gmail.com>
2023-03-13 22:59:52 +0545

    Fix Linter Errors

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,4 @@
 class Food < ApplicationRecord
   has_many :recipe_foods
-  belongs_to :author, class_name: "User"
+  belongs_to :author, class_name: 'User'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,4 @@ class User < ApplicationRecord
   has_many :inventories, foreign_key: :author_id
   has_many :recipes, foreign_key: :author_id
   has_many :authored_foods, class_name: 'Food', foreign_key: 'author_id'
-
 end
