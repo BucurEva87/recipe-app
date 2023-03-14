e370048d69c941c650230cbfbf5c5ac286f74f4b
Krishna <libraryismyparadise@gmail.com>
2023-03-14 14:33:36 +0545

    Fix Linters

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,8 +1,7 @@
 class Ability
   include CanCan::Ability
- #  
   def initialize(user)
-    can :add_new_food, Recipe do |recipe| 
+    can :add_new_food, Recipe do |recipe|
       recipe.user == user
     end
   end
