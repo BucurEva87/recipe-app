43fe6def8005ddc9d7217e1c0150ec06fa956dc7
BucurEva87 <bucur.eva87@gmail.com>
2023-03-18 14:47:15 +0200

    Add bullet gem support

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -79,4 +79,12 @@ Rails.application.configure do
 
   # Reload the browser automatically whenever files change
   config.middleware.insert_after(ActionDispatch::Static, Rack::LiveReload)
+
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.alert = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+  end
 end
